37b98521c1936b2563d5a97ab58de28dbc3621f6
yambrosio <YENER.AMBROSIO@TOMZAGROUP.COM>
2022-08-25 11:24:01 -0600

    Init commit: Configuración de estructura del proyecto, se agrego las librerias iniciales al proyecto

---
+++ app/src/main/java/com/fambrosio/authfirebase/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fambrosio.authfirebase.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fambrosio.authfirebase.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
